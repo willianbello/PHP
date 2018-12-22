90957f2aa1aecf78f62e39264b33e1582c0ad460
willianbello <willianbello.s@gmail.com>
2018-12-22 18:21:31 -0200

    criando array multidimensional para simbolizar um tabuleiro de xadrez

---
+++ _arrayMultidimensional/tabuleiro.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variaveis</title>
+</head>
+
+<body>
+
+    <?php 
+        
+        $tabuleiro[1]['a'] = 'torre branca';
+        $tabuleiro[1]['b'] = 'cavalo branco';
+        $tabuleiro[1]['c'] = 'bispo branco';
+        $tabuleiro[1]['d'] = 'rainha branca';
+        $tabuleiro[1]['e'] = 'rei branco';
+        $tabuleiro[1]['f'] = 'bispo branco';
+        $tabuleiro[1]['g'] = 'cavalo branco';
+        $tabuleiro[1]['h'] = 'torre branca';
+
+        $tabuleiro[2]['a'] = 'peao branco';
+        $tabuleiro[2]['b'] = 'peao branco';
+        $tabuleiro[2]['c'] = 'peao branco';
+        $tabuleiro[2]['d'] = 'peao branco';
+        $tabuleiro[2]['e'] = 'peao branco';
+        $tabuleiro[2]['f'] = 'peao branco';
+        $tabuleiro[2]['g'] = 'peao branco';
+        $tabuleiro[2]['h'] = 'peao branco';
+
+        $tabuleiro[3]['a'] = '';
+        $tabuleiro[3]['b'] = '';
+        $tabuleiro[3]['c'] = '';
+        $tabuleiro[3]['d'] = '';
+        $tabuleiro[3]['e'] = '';
+        $tabuleiro[3]['f'] = '';
+        $tabuleiro[3]['g'] = '';
+        $tabuleiro[3]['h'] = '';
+
+        $tabuleiro[4]['a'] = '';
+        $tabuleiro[4]['b'] = '';
+        $tabuleiro[4]['c'] = '';
+        $tabuleiro[4]['d'] = '';
+        $tabuleiro[4]['e'] = '';
+        $tabuleiro[4]['f'] = '';
+        $tabuleiro[4]['g'] = '';
+        $tabuleiro[4]['h'] = '';
+
+        $tabuleiro[5]['a'] = '';
+        $tabuleiro[5]['b'] = '';
+        $tabuleiro[5]['c'] = '';
+        $tabuleiro[5]['d'] = '';
+        $tabuleiro[5]['e'] = '';
+        $tabuleiro[5]['f'] = '';
+        $tabuleiro[5]['g'] = '';
+        $tabuleiro[5]['h'] = '';
+
+        $tabuleiro[6]['a'] = '';
+        $tabuleiro[6]['b'] = '';
+        $tabuleiro[6]['c'] = '';
+        $tabuleiro[6]['d'] = '';
+        $tabuleiro[6]['e'] = '';
+        $tabuleiro[6]['f'] = '';
+        $tabuleiro[6]['g'] = '';
+        $tabuleiro[6]['h'] = '';
+
+        $tabuleiro[7]['a'] = 'peao preto';
+        $tabuleiro[7]['b'] = 'peao preto';
+        $tabuleiro[7]['c'] = 'peao preto';
+        $tabuleiro[7]['d'] = 'peao preto';
+        $tabuleiro[7]['e'] = 'peao preto';
+        $tabuleiro[7]['f'] = 'peao preto';
+        $tabuleiro[7]['g'] = 'peao preto';
+        $tabuleiro[7]['h'] = 'peao preto';
+
+        $tabuleiro[8]['a'] = 'torre preta';
+        $tabuleiro[8]['b'] = 'cavalo preto';
+        $tabuleiro[8]['c'] = 'bispo preto';
+        $tabuleiro[8]['d'] = 'rainha preta';
+        $tabuleiro[8]['e'] = 'rei preto';
+        $tabuleiro[8]['f'] = 'bispo preto';
+        $tabuleiro[8]['g'] = 'cavalo preto';
+        $tabuleiro[8]['h'] = 'torre preta';
+
+        print_r($tabuleiro);
+    ?>
+
+</body>
+
+</html>
